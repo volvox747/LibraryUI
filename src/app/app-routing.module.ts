4a1d104267eeb6a482785ca624d01e3437a08159
Benson Thomas <tbenson@ch-telliant.com>
2022-07-28 20:40:33 +0530

    Added route for books page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path:'',component:LoginPageComponent},
-  {path:'/register',component:RegisterComponent}
+  {path:'register',component:RegisterComponent},
+  {path:'books',component:BooksComponent}
 ];
 
 @NgModule({
