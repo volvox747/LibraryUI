94aa5f3db31079e1cdd1ecd6372a76e707e75c23
Benson Thomas <tbenson@ch-telliant.com>
2022-07-28 15:55:47 +0530

    Added route for register form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  {path:'',component:LoginPageComponent}
+  {path:'',component:LoginPageComponent},
+  {path:'/register',component:RegisterComponent}
 ];
 
 @NgModule({
