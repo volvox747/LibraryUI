30ffbfba675d0824ccb59201bf9339071143a4f8
Benson Thomas <tbenson@ch-telliant.com>
2022-07-29 13:12:02 +0530

    Added route for details page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookDetailsComponent } from './books/book-details/book-details.component';
 import { BooksComponent } from './books/books.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterComponent } from './register/register.component';
@@ -7,7 +8,8 @@ import { RegisterComponent } from './register/register.component';
 const routes: Routes = [
   {path:'',component:LoginPageComponent},
   {path:'register',component:RegisterComponent},
-  {path:'books',component:BooksComponent}
+  {path:'books',component:BooksComponent},
+  {path:'books/:bookId',component:BookDetailsComponent}
 ];
 
 @NgModule({
