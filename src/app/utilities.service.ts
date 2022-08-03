56c5e62551912e2e60cf0f9541f90a236a7e4c16
Volvox747 <bensonthomascr7@gmail.com>
2022-08-03 19:28:10 +0530

    Added show flags for edit,delete & add book

--- src/app/utilities.service.ts
+++ src/app/utilities.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,7 @@ import { Injectable } from '@angular/core';
 export class UtilitiesService {
 
   constructor() { }
+  showOnDelete:boolean=false
+  showOnAdd:boolean=false
+  showOnEdit=new Subject<boolean>();
 }
