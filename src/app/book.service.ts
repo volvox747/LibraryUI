3fad79e291ff16531f0875f4c54406fecd2835b8
Volvox747 <bensonthomascr7@gmail.com>
2022-08-10 19:39:20 +0530

    Added Alert msg to ADD,UPDATE,DELETE and REQUEST Book from user

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -1,61 +1,100 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { BookSchema } from './models/book.model';
-import { RequestSchema } from "./models/request.model";
-import { map, Subject } from "rxjs";
+import { RequestSchema } from './models/request.model';
+import { catchError, map, Subject, tap, throwError } from 'rxjs';
 import { RequestBookSchema } from './models/requestbook.model';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class BookService {
-
-  books:BookSchema[];
-  bookDetails=new Subject<BookSchema>();
-  constructor(private http:HttpClient) { }
-
-  postAddBook(bookData:BookSchema)
-  {
-    return this.http.post('https://localhost:44309/add-book',bookData)
-  }
+export class BookService
+{
+  books: BookSchema[];
+  bookDetails = new Subject<BookSchema>();
+  constructor(private http: HttpClient) { }
 
   // Get All Books Details
   getData()
   {
-      return this.http
-      .get<BookSchema[]>("https://localhost:44309/books").pipe(map(res=>this.books=res))
+    return this.http
+      .get<BookSchema[]>('https://localhost:44309/books')
+      .pipe(map((res) => (this.books = res)))
+      .pipe(catchError((err) => throwError(() => new Error(err))));
   }
 
   getRequestBooksForAdmin()
   {
-    return this.http.get("https://localhost:44309/request-books")
+    return this.http
+      .get<RequestBookSchema[]>('https://localhost:44309/request-books')
+      .pipe(catchError((err) => throwError(() => new Error(err))));
   }
 
-
-  getRequestBooksForUser(loginId:string)
-  {
-    return this.http.get<RequestBookSchema[]>(`https://localhost:44309/request-books/${loginId}`)
-  }
-  
-  // post the request of book along with loginId and get requested msg
-
-  postrequestBook(requestData:RequestSchema)
-  {
-      return this.http
-          .post<string>('https://localhost:44309/request-book',requestData)
-  }
-
-  // update book details
-  updateBook(updatedData:BookSchema)
+  getRequestBooksForUser(loginId: string)
   {
     return this.http
-        .put<string>("https://localhost:44309/update-book",updatedData)
+      .get<RequestBookSchema[]>(
+        `https://localhost:44309/request-books/${loginId}`
+      )
+      .pipe(catchError((err) => throwError(() => new Error(err))));
   }
 
-  deleteBook(bookId:string)
+  // post the request of book along with loginId and get requested msg
+
+  postrequestBook(requestData: RequestSchema)
   {
-    return this.http.delete(`https://localhost:44309/delete-book/${bookId}`)
+    return this.http
+      .post<string>('https://localhost:44309/request-book', requestData)
+      .pipe(
+        catchError((err: HttpErrorResponse) =>
+        {
+          console.log(err);
+          return throwError(() => err);
+        })
+      );
   }
 
+  // add a new book into the database
+
+  postAddBook(bookData: BookSchema)
+  {
+    return this.http
+      .post('https://localhost:44309/add-book', bookData)
+      .pipe(
+        catchError((err: HttpErrorResponse) =>
+        {
+          console.log(err.status);
+          return throwError(() => err);
+        })
+      );
+  }
+
+  // update book details in the database
+  updateBook(updatedData: BookSchema)
+  {
+    return this.http
+      .put<string>('https://localhost:44309/update-book', updatedData)
+      .pipe(
+        catchError((err:HttpErrorResponse) =>
+        {
+          console.log(err);
+          return throwError(() => err);
+        })
+      );
+  }
+
+  // deleting a book from the database
+  deleteBook(bookId: string)
+  {
+    return this.http
+      .delete(`https://localhost:44309/delete-book/${bookId}`)
+      .pipe(
+        tap((res) => console.log(res)),
+        catchError((err:HttpErrorResponse) =>
+        {
+          console.log(err);
+          return throwError(() => err);
+        })
+      );
+  }
 }
--- src/app/edit-book/edit-book.component.ts
+++ src/app/edit-book/edit-book.component.ts
@@ -1,37 +1,58 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { BookService } from '../book.service';
-import { BookSchema } from '../models/book.model';
-import { UtilitiesService } from '../utilities.service';
+import {HttpErrorResponse} from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {BookService} from '../book.service';
+import {BookSchema} from '../models/book.model';
+import {UtilitiesService} from '../utilities.service';
 
 @Component({
   selector: 'app-edit-book',
   templateUrl: './edit-book.component.html',
   styleUrls: ['./edit-book.component.css']
 })
-export class EditBookComponent implements OnInit {
+export class EditBookComponent implements OnInit
+{
 
-  bookData:BookSchema
-  constructor(private book:BookService,private route:ActivatedRoute,private router:Router,private utilities:UtilitiesService) { }
+  bookData: BookSchema
+  constructor(private book: BookService, private route: ActivatedRoute, private router: Router, private utilities: UtilitiesService) {}
+
+  errorMsg:{flag:boolean,error:{}} ={flag:false,error:{}};
 
   ngOnInit(): void 
   {
     // filter book from book details array using id
-    this.bookData=this.book.books.filter(book=>book.bookId===this.route.snapshot.params.bookId)[0];
-    console.log(this.bookData);
-    
+    this.bookData = this.book.books.filter(book => book.bookId === this.route.snapshot.params.bookId)[0];
   }
 
-  onEditBook(data:NgForm)
+  onEditBook(data: NgForm)
   {
+    // adding unique id to book
     data.value.bookId=this.bookData.bookId
-    this.book.updateBook(data.value).subscribe(res=>{
-      if(res==="Book Updated Successfully")
+    
+    this.book.updateBook(data.value).subscribe(
       {
-        this.router.navigateByUrl(`/books/${this.route.snapshot.params.bookId}`,{state:{data:true,editedBookData:this.bookData}})
-      }
-    });
+        next: (res) =>
+        {
+          console.log(res);
+
+          if (res === "Book Updated Successfully")
+          {
+            this.router.navigateByUrl(`/books/${this.route.snapshot.params.bookId}`, {state: {data: true, editedBookData: this.bookData}})
+          }
+        },
+        error: (err:HttpErrorResponse) =>
+        {
+          console.log(err.error);
+          this.errorAlertDisplay(err)
+        }
+      });
+  }
+  private errorAlertDisplay(err: HttpErrorResponse)
+  {
+    this.errorMsg.flag = true
+    this.errorMsg.error = err.error
+    setTimeout(() => this.errorMsg.flag = false, 2000);
   }
 
 }
